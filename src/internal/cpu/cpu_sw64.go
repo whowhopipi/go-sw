79efde32efe0720945c0690f123092114d7e5ff2
zxw <zxw@163.com>
2021-06-23 14:05:43 +0800

    Add CPU-related informationc for sw64.

--- src/internal/cpu/cpu_sw64.go
+++ src/internal/cpu/cpu_sw64.go
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build sw64
+
 package cpu
 
-//zxw new change
 const CacheLinePadSize = 128
+
+func doinit() {
+}
--- src/vendor/golang.org/x/sys/cpu/cpu_linux_sw64.go
+++ src/vendor/golang.org/x/sys/cpu/cpu_linux_sw64.go
@@ -1,12 +1,11 @@
-// Copyright 2018 The Go Authors. All rights reserved.
+// Copyright 2020 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux
 // +build sw64
 
 package cpu
 
-const cacheLineSize = 128
-
-func initOptions() {
+func doinit() {
 }
